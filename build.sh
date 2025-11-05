c83a3e67b1be82eaab136d523ee686732b4a11e1
Christian Bueno <christianbueno.1@gmail.com>
2025-11-04 21:37:02 -0500

    ops: update build script

--- build.sh
+++ build.sh
@@ -9,16 +9,29 @@ BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD | tr '/' '-')
 echo "Building image for commit: $COMMIT_SHA on branch: $BRANCH_NAME"
 # variables
 IMAGE_NAME="chris-fedora43"
+USER_NAME="christianbueno1"
 
 # build the image with podman
-podman build -t docker.io/christianbueno1/$IMAGE_NAME:$COMMIT_SHA -f Containerfile .
+podman build -t docker.io/$USER_NAME/$IMAGE_NAME:$COMMIT_SHA -f Containerfile .
 # tag with branch name
-podman tag docker.io/christianbueno1/$IMAGE_NAME:$COMMIT_SHA docker.io/christianbueno1/$IMAGE_NAME:$BRANCH_NAME
+podman tag docker.io/$USER_NAME/$IMAGE_NAME:$COMMIT_SHA docker.io/$USER_NAME/$IMAGE_NAME:$BRANCH_NAME
 # tag with latest if on main branch
 if [ "$BRANCH_NAME" == "main" ]; then
-    podman tag docker.io/christianbueno1/$IMAGE_NAME:$COMMIT_SHA docker.io/christianbueno1/$IMAGE_NAME:latest
+    podman tag docker.io/$USER_NAME/$IMAGE_NAME:$COMMIT_SHA docker.io/$USER_NAME/$IMAGE_NAME:latest
 fi
 
 # print for debugging the images built
 echo "Built images:"
-podman images --filter=reference="docker.io/christianbueno1/$IMAGE_NAME*"
+podman images --filter=reference="docker.io/$USER_NAME/$IMAGE_NAME*"
+
+# authenticate to docker hub, use password-stdin and file ~/Documents/docker-pat.txt
+echo "Authenticating to Docker Hub..."
+podman login -u $USER_NAME --password-stdin docker.io < ~/Documents/docker-pat.txt
+
+# push the images
+echo "Pushing images to Docker Hub..."
+podman push docker.io/$USER_NAME/$IMAGE_NAME:$COMMIT_SHA
+podman push docker.io/$USER_NAME/$IMAGE_NAME:$BRANCH_NAME
+if [ "$BRANCH_NAME" == "main" ]; then
+    podman push docker.io/$USER_NAME/$IMAGE_NAME:latest
+fi
